c212f0283f4e9656222f2136fabdd301ec8bbbaf
anagharumade <anagharumade.8@gmail.com>
2017-10-14 15:08:40 -0400

    Splitting training and testing data

--- split_train_test.py
+++ split_train_test.py
@@ -28,3 +28,5 @@ labelencoder = LabelEncoder()
 Y = labelencoder.fit_transform(Y)
 
 #Splitting into training and test
+from sklearn.cross_validation import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2)
