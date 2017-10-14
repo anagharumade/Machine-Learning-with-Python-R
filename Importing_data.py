3668630c32c9f1b943ee919a2ce364a10e6d4237
anagharumade <anagharumade.8@gmail.com>
2017-10-14 19:02:07 -0400

    removing files

--- Importing_data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv('Data.csv')
-train_ind = data.iloc[:, :-1]
-countries = data.to_csv(columns = ['Country'], index = False) #Creates a string. can be split by spaces to obtain words
-countries = data[['Country','Age']] # Creating a data frame using column headers
-print(countries)
